e6e67179db54efba452b62f6f627c1140756da5c
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-10 13:55:36 +0700

    mengubah nama class contoh jadi contoh1

--- Assets/Scripts/Contoh1.cs
+++ Assets/Scripts/Contoh1.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Contoh : MonoBehaviour
+public class Contoh1 : MonoBehaviour
 {
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
