e8866c2418891236ac3692c402ff5da2ebe46091
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-10 17:07:10 +0700

    belajar fundamental c#

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+    // Vector2 position1 = new Vector2(5, 1);
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int kali(int a, int b, int c)
+    {
+        return a * b * c;
+    }
+
     void Start()
     {
-        
+        Debug.Log(kali(1, 2, 3));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log("Hello World");        
     }
 }
