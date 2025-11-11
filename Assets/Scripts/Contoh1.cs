8f90f99b06c9f334efa6b7471d33ff0444dbac3c
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-11 10:57:17 +0700

    Latihan Array #1
    coba menambahkan atribut [SerializeField] di array biar element nya bisa diinput dari inspector

--- Assets/Scripts/Contoh1.cs
+++ Assets/Scripts/Contoh1.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class Contoh1 : MonoBehaviour
 {
-    
+    [SerializeField]
+    string[] Nama = new string[3];
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(Nama[0]);
+        Debug.Log(Nama[1]);
+        Debug.Log(Nama[2]);
+
     }
 
     // Update is called once per frame
