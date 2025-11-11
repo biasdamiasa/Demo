24bb8eb72fd6370a025a02c59b1d108efa2ee6c9
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-11 11:58:04 +0700

    Latihan List #1
    Pencarian object di dalam list menggunakan method find

--- Assets/Scripts/Contoh1.cs
+++ Assets/Scripts/Contoh1.cs
@@ -1,25 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Contoh1 : MonoBehaviour
 {
     [SerializeField]
-    Player[] ArrayPlayer;
+    List<Player> ListPlayer = new List<Player>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < ArrayPlayer.Length; i++)
-        {
-            Debug.Log(ArrayPlayer[i].Name);
-            Debug.Log(ArrayPlayer[i].Level);
-            Debug.Log(ArrayPlayer[i].Health);
-        }
-        
-        foreach (Player player in ArrayPlayer)
-        {
-            Debug.Log(player.Name);
-            Debug.Log(player.Level);
-            Debug.Log(player.Health);
-        }
+        Player player = ListPlayer.Find(item => item.Level > 5);
+
+        Debug.Log(player.Name);
     }
 
     // Update is called once per frame
