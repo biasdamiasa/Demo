3dac42da1d54090d0aefed0eac9df3a35477ec2c
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-11 10:36:15 +0700

    tambah serializable
    serializable field agar fields bisa diisi nilainya di unity

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,29 +2,12 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
-    
-    
+
+    [SerializeField]
+    Player player;
     void Start()
     {
-        Car car1 = new Car("Avanza", "Black");
-        Car car2 = new Car("Sigra", "Orange");
-
-        Debug.Log("Car 1");
-        Debug.Log("Merk: " + car1.merk);
-        Debug.Log("Warna: " + car1.warna);
-        Debug.Log("Kecepatan: " + car1.speed);
-        car1.start();
-        car1.accelerate(40);
-        car1.brake();
-
-        Debug.Log("Car 2");
-        Debug.Log("Merk: " + car2.merk);
-        Debug.Log("Warna: " + car2.warna);
-        Debug.Log("Kecepatan: " + car2.speed);
-        car2.start();
-        car2.accelerate(30);
-        car2.brake();
-
+        
     }
 
     
