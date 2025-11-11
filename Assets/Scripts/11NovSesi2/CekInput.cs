333220e1d2a412179955fd1b3f8bb7a69d68da9c
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-11 14:35:24 +0700

    destroy object
    destroy object with tag

--- Assets/Scripts/11NovSesi2/CekInput.cs
+++ Assets/Scripts/11NovSesi2/CekInput.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 public class CekInput : MonoBehaviour
 {
     public GameObject _munculHilang;
-
-    
     public GameObject _untukSpawn;
+    public GameObject _targetDestroy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,15 +30,23 @@ public class CekInput : MonoBehaviour
             Debug.Log("Tombol P ditekan");
             _munculHilang.SetActive(false);
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Tombol Enter ditekan");
 
             // Vector3 spawnPosition = new Vector3(2, 2, 0);
             // Quaternion spawnRotation = Quaternion.Euler(0, 0, 45);
-            Instantiate(_untukSpawn, new Vector3(Random.Range(-5,5), Random.Range(-5,5), 0), Quaternion.Euler(0, 0, 0));
+            Instantiate(_untukSpawn, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.Euler(0, 0, Random.Range(0,360)));
 
+        }
+        
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            GameObject hancurkan = GameObject.FindGameObjectWithTag("sasaran");
+            Debug.Log("Tombol M ditekan");
+            Destroy(_targetDestroy);
+            Destroy(hancurkan);
         }
     }
 }
