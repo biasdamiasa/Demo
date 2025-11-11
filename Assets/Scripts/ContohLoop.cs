7c5cc52a8fa20775b032c72589da6c8f9f3320e3
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-11 09:21:37 +0700

    challenge loop#1
    faktorial

--- Assets/Scripts/ContohLoop.cs
+++ Assets/Scripts/ContohLoop.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class ContohLoop : MonoBehaviour
 {
-    
+    int result = 1;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < 3; i++)
+        for(int i = 4; i >= 1; i--)
         {
-            Debug.Log("Pengulangan ke-" + i);
+            result *= i;
         }
+
+        Debug.Log(result);
     }
+    
 
     // Update is called once per frame
     void Update()
