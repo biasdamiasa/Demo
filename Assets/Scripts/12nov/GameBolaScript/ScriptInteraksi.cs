5221f8901866027e80a00cfd7fa0808ce995e645
biasdamiasa <bias@smktelkom-mlg.sch.id>
2025-11-13 09:12:18 +0700

    code review Game Kotak Bergerak

--- Assets/Scripts/12nov/GameBolaScript/ScriptInteraksi.cs
+++ Assets/Scripts/12nov/GameBolaScript/ScriptInteraksi.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public class ScriptInteraksi : MonoBehaviour
 {
+    // deklarasi referensi ke class ScriptSquareMove
     public ScriptSquareMove scriptSquareMove;
+
+    //deklarasi variabel score
     public int score;
+
+    //deklarasi referensi object ke gameObject ScoreText
     public TMP_Text ScoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,21 +23,25 @@ public class ScriptInteraksi : MonoBehaviour
 
     }
 
+    // method yang dipanggil Button Atas untuk mengubah gerak Square ke atas
     public void GerakAtas()
     {
         scriptSquareMove.arahgerak = ArahGerak.atas;
     }
 
+    // method yang dipanggil Button Bawah untuk mengubah gerak Square ke bawah
     public void GerakBawah()
     {
         scriptSquareMove.arahgerak = ArahGerak.bawah;
     }
 
+    // method yang dipanggil Button Atas untuk mengubah gerak Square ke kanan
     public void GerakKanan()
     {
         scriptSquareMove.arahgerak = ArahGerak.kanan;
     }
     
+    // method yang dipanggil Button Atas untuk mengubah gerak Square ke kiri
     public void GerakKiri()
     {
         scriptSquareMove.arahgerak = ArahGerak.kiri;
--- Assets/Scripts/12nov/GameBolaScript/ScriptSquareMove.cs
+++ Assets/Scripts/12nov/GameBolaScript/ScriptSquareMove.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 
 public class ScriptSquareMove : MonoBehaviour
-{
+{   
+    //deklarasi  
     public ArahGerak arahgerak = ArahGerak.kanan;
+    
+    //deklarasi referensi ke class ScriptInteraksi
     public ScriptInteraksi scriptInteraksi;
+
+    // deklarasi variabel speed untuk gerak kotak
     public float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +20,11 @@ public class ScriptSquareMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // mengupdate gerak object berdasarkan nilai arahgerak kotak
+        // jika nilai arahgerak adalah atas, maka akan mengupdate gerak hanya di koordinat y (+)
+        // jika nilai arahgerak adalah bawah, maka akan mengupdate gerak hanya di koordinat y (-)
+        // jika nilai arahgerak adalah kanan, maka akan mengupdate gerak hanya di koordinat x (+)
+        // jika nilai arahgerak adalah kiri, maka akan mengupdate gerak hanya di koordinat x (-)
         switch (arahgerak)
         {
             case ArahGerak.atas:
@@ -32,6 +42,11 @@ public class ScriptSquareMove : MonoBehaviour
         }
     }
 
+
+    // deklarasi method untuk mendeteksi collision
+    // mengecek apabila yang mengalami collision adalah gameObject dengan Tag "Dinding"
+    // jika terdeteksi menabrak gameoBject dengan tag "Dinding", dengan arahgerak kanan maka arahgerak akan diubah nilainya kiri
+    // dst untuk mengubah arahgerak square setiap terdeteksi tabrakan
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Dinding"))
@@ -53,17 +68,25 @@ public class ScriptSquareMove : MonoBehaviour
                 arahgerak = ArahGerak.atas;
             }
             Debug.Log("Menabrak dinding");
+
+            // memanggil method TambahSkor setelah mendeteksi tabrakan dan mengubah arahgerak
             TambahSkor();
         }
     }
 
+
+    //untuk menambah skor dan mengupdate teks gameObject ScoreText
     public void TambahSkor()
     {
+        //menambah nilai variabel score sebanyak 1
         scriptInteraksi.score++;
+
+        //mengubah teks ScoreText dengan skor terbaru
         scriptInteraksi.ScoreText.text = "Skor: " + scriptInteraksi.score;
     }
 }
 
+//mendeklarasi enum untuk 4 macam arah gerak
 public enum ArahGerak
 {
     atas,
